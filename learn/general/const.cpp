4b453b1f96e160811dd7e26522bf67011cc9ab34
girishsontakke <girishsontakke7@gmail.com>
2020-07-09 08:56:43 +0530

    diff. between const char *ptr, char const *ptr

--- learn/general/const.cpp
+++ learn/general/const.cpp
@@ -2,11 +2,50 @@
 using namespace std;
 #define X 10 // defining constants by macros
 
-int main() {
+void const_ptr_demo(){
+
+    char a ='A', b ='B'; 
+    char *const ptr = &a; 
+    printf( "Value pointed to by ptr: %c\n", *ptr); 
+    printf( "Address ptr is pointing to: %d\n\n", ptr); 
+  
+    //ptr = &b; illegal statement (assignment of read-only variable ptr) 
+  
+    // changing the value at the address ptr is pointing to 
+    *ptr = b;  
+    printf( "Value pointed to by ptr: %c\n", *ptr); 
+    printf( "Address ptr is pointing to: %d\n", ptr); 
+}
+
+void const_char_demo(){
     const int x = 5; //symbolic constant
     enum { y = 100}; //enum constant
 
-    auto s = 4; // -std=c++11
-    cout << s << endl ;
-    return 0;
+    char a ='A', b ='B'; 
+    const char *ptr = &a; 
+      
+    // *ptr = b; illegal statement (assignment of read-only location *ptr); 
+      
+    // ptr can be changed 
+    printf( "value pointed to by ptr: %c\n", *ptr); 
+    ptr = &b; 
+    printf( "value pointed to by ptr: %c\n", *ptr); 
+
+}
+
+void const_char_const_ptr_demo(){
+
+    char a ='A', b ='B'; 
+    const char *const ptr = &a; 
+      
+    printf( "Value pointed to by ptr: %c\n", *ptr); 
+    printf( "Address ptr is pointing to: %d\n\n", ptr); 
+  
+    // ptr = &b; illegal statement (assignment of read-only variable ptr) 
+    // *ptr = b; illegal statement (assignment of read-only location *ptr) 
+  
+}
+
+int main() {
+    const_char_const_ptr_demo()
 }
