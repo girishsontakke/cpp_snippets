0bc889cc6a245b401ed4c1dd3d39fa08c8e8b6f1
girishsontakke <girishsontakke7@gmail.com>
2020-09-25 19:30:58 +0530

    function in cpp

--- learn/general/functions.cpp
+++ learn/general/functions.cpp
@@ -2,15 +2,18 @@
 #include<cmath>
 using namespace std;
 
+// prototype or declaration
+int add(int a, int b); //-> formal parameters
 
 int main()
 {
-    int base, exponent;
-    cout << "Enter a base:";
-    cin >> base;
-    cout << "Enter a exponent:";
-    cin >> exponent;
-    double power = pow(base, exponent);
-    cout << power;
-    return 0;
+    int a=1, b=2;
+
+    // function calling
+    cout << add(1, 2) << endl; //-> actual parameters
+}
+
+// function definition
+int add(int a, int b){
+    return a + b;
 }
